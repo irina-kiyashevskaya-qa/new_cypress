88d2450e60d3952e10f5783be250501294779abc
German Dolnikov <german@dolnikov.ru>
2024-04-28 23:40:18 +0300

    best practice

--- cypress/e2e/lesson.cy.js
+++ cypress/e2e/lesson.cy.js
@@ -1,33 +1,47 @@
-
 describe('Проверка авторизации', function () {
 
-   beforeEach('Начало теста', function () {
-         cy.visit('/');
-         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
-           });
-
-   afterEach('Конец теста', function () {
-         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
-        });
-
    it('Верный пароль и верный логин', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
         cy.get('#mail').type('german@dolnikov.ru');
         cy.get('#pass').type('iLoveqastudio1');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
     })
 
-      it('Верный логин и неверный пароль', function () {
+    it('Верный логин и неверный пароль', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
         cy.get('#mail').type('german@dolnikov.ru');
         cy.get('#pass').type('iLoveqastudio2');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
 
-
+    it('Валидация на наличие @', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+    it('Восстановление пароля', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
     })
 })
 
 
-// npx cypress run --spec cypress/e2e/poke.cy.js --browser chrome
+// запуск через теринал: npx cypress run --spec cypress/e2e/poke.cy.js --browser chrome
